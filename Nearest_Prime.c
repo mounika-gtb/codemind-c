c0bf01a9ff2e589f4e627c892479ff1d50e9203b
mounika-gtb <84364046+mounika-gtb@users.noreply.github.com>
2022-11-09 08:21:43 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,j,k=0,c=0,mx=0,mn=0,m=0;     scanf("%d",&n);     int a ,temp=n;     while(temp--)     {         scanf("%d",&a);         c=0;         k=0;         for(i=0;i<a;i++)         {             c=0;             for(j=2;j<a+k;j++)             {                 if((a+k)%j==0)                 {                     c++;                 }             }             if(c==0)             {                 mx=a+k;                 break;             }             k++;         }         c=0;         m=0;         for(i=0;i<a;i++)         {             c=0;             for(j=2;j<a-m;j++)             {                 if((a-m)%j==0)                 {                     c++;                 }             }             if(c==0)             {                 mn=a-m;                 break;             }             m++;         }         int min=a-mn,max=mx-a;         if(min>max)         printf("%d ",mx);         else         printf("%d ",mn);     } }

---
+++ Nearest_Prime.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,j,k=0,c=0,mx=0,mn=0,m=0;
+    scanf("%d",&n);
+    int a ,temp=n;
+    while(temp--)
+    {
+        scanf("%d",&a);
+        c=0;
+        k=0;
+        for(i=0;i<a;i++)
+        {
+            c=0;
+            for(j=2;j<a+k;j++)
+            {
+                if((a+k)%j==0)
+                {
+                    c++;
+                }
+            }
+            if(c==0)
+            {
+                mx=a+k;
+                break;
+            }
+            k++;
+        }
+        c=0;
+        m=0;
+        for(i=0;i<a;i++)
+        {
+            c=0;
+            for(j=2;j<a-m;j++)
+            {
+                if((a-m)%j==0)
+                {
+                    c++;
+                }
+            }
+            if(c==0)
+            {
+                mn=a-m;
+                break;
+            }
+            m++;
+        }
+        int min=a-mn,max=mx-a;
+        if(min>max)
+        printf("%d
+",mx);
+        else
+        printf("%d
+",mn);
+    }
+}
