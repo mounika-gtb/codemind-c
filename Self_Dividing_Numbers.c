941395c83163ac6d47237c8dfb2817c84196323f
mounika-gtb <84364046+mounika-gtb@users.noreply.github.com>
2022-11-09 08:13:58 +0530

    #include<stdio.h> int main() {     int a,b,temp,i,d;     scanf("%d%d",&a,&b);     for(i=a;i<=b;i++)     {         temp=i;         while(temp!=0)         {             d=temp%10;             if(d==0 || i%d!=0)             {                 break;             }             temp=temp/10;         }         if(temp==0)         {             printf("%d ",i);         }     }     return 0; }

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,temp,i,d;
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        temp=i;
+        while(temp!=0)
+        {
+            d=temp%10;
+            if(d==0 || i%d!=0)
+            {
+                break;
+            }
+            temp=temp/10;
+        }
+        if(temp==0)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
