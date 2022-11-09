9e2ed4d4d7a5a785d77e2d3e2475e2aa6b3e3b8f
mounika-gtb <84364046+mounika-gtb@users.noreply.github.com>
2022-11-09 08:51:57 +0530

    #include <stdio.h> int main() {   int i, n;   int t1 = 0, t2 = 1;   int nt = t1 + t2;   scanf("%d", &n);   printf("%d %d ", t1, t2);   for (i = 3; i <= n; ++i) {     printf("%d ", nt);     t1 = t2;     t2 = nt;     nt = t1 + t2;   }   return 0; }

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int i, n;
+  int t1 = 0, t2 = 1;
+  int nt = t1 + t2;
+  scanf("%d", &n);
+  printf("%d %d ", t1, t2);
+  for (i = 3; i <= n; ++i) {
+    printf("%d ", nt);
+    t1 = t2;
+    t2 = nt;
+    nt = t1 + t2;
+  }
+  return 0;
+}
